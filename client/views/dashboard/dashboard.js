f51087a18b825abe2afc47ffa2759e28f16a0614
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-11-10 09:19:00 +0100

    Possible to remove a doc

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,11 +1,24 @@
 Template.Dashboard.events({
 	'click #btn-newDoc': function(event, tmpl) {
-		var doc = {title: 'Mitt nye dokument', lastChanged: new Date()};
+		var doc = {title: 'Mitt nye dokument', lastChanged: new Date(), userId: Meteor.userId()};
 		var _id = Documents.insert(doc);
 		Router.go(Router.path('Document', {_id: _id}));
 
 	},
 	'click .row-item' : function(event, tmpl) {
 		Router.go(Router.path('Document', {_id: this._id}));
-	}
+	},
+  'click #btn-editDoc': function(event, tmpl) {
+    Router.go(Router.path('Document', {_id: this._id}));
+  },
+  'click #btn-remove': function(event, tmpl) {
+    Documents.remove({_id: this._id}, function(error) {
+      if(error) {
+        Notifications.warn('Feil', error.message);
+      }
+      else {
+        Notifications.success('Dokument slettet', 'Dokument slettet');
+      }
+    });
+  }
 });
