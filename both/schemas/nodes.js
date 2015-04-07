de6e231fc497576756458c746fa9cbfce7070bfc
Andreas Drivenes <andreas.drivenes@gmail.com>
2015-04-07 14:10:00 +0200

    Reindent and add Intellij to gitignore

--- both/schemas/nodes.js
+++ both/schemas/nodes.js
@@ -4,100 +4,100 @@ var subscriptions = {};
 
 NodesScheme = new SimpleSchema({
 
-	// A document title
-	title: {
-		type: String,
-		label: function() {
-			return "Tittel";
-		},
-		optional: false,
-		min: 3,
-		max: 100,
-		autoform: {
-			placeholder: "schemaLabel"
-		}
-	},
+  // A document title
+  title: {
+    type: String,
+    label: function() {
+      return "Tittel";
+    },
+    optional: false,
+    min: 3,
+    max: 100,
+    autoform: {
+      placeholder: "schemaLabel"
+    }
+  },
 
-	// A description that describe the content
-	description: {
-		type: String,
-		label: function() {
-			return "Beskrivelse";
-		},
-		optional: true,
-		max: 500,
-		autoform: {
-			placeholder: "schemaLabel"
-		}
-	},
+  // A description that describe the content
+  description: {
+    type: String,
+    label: function() {
+      return "Beskrivelse";
+    },
+    optional: true,
+    max: 500,
+    autoform: {
+      placeholder: "schemaLabel"
+    }
+  },
 
-	// Tags that adds a dimension to the description and title fields
-	tags: {
-		type: [String],
-		label: function() {
-			return "Emneknagger";
-		},
-		optional: true,
-		autoform: {
-			placeholder: "schemaLabel",
-			type: "selectize",
-			afFieldInput: {
-				multiple: true,
-				selectizeOptions: {
-					delimiter: ',',
-					persist: false,
+  // Tags that adds a dimension to the description and title fields
+  tags: {
+    type: [String],
+    label: function() {
+      return "Emneknagger";
+    },
+    optional: true,
+    autoform: {
+      placeholder: "schemaLabel",
+      type: "selectize",
+      afFieldInput: {
+        multiple: true,
+        selectizeOptions: {
+          delimiter: ',',
+          persist: false,
 
-					// Can create new tags
-					create: function(input) {
+          // Can create new tags
+          create: function(input) {
 
-						Tags.insert({ value: input, text: input });
+            Tags.insert({ value: input, text: input });
 
-						return {
-							value: input,
-							text: input
-						}
-					},
+            return {
+              value: input,
+              text: input
+            }
+          },
 
-					// How the tags suggestions are rendered
-					render: {
-						option: function(item, escape) {
-							return '<div>' +
-							'<span class="title">' +
-							'<span class="name">' + item.text + '</span>' +
-							'</span>' +
-							'</div>';
-						}
-					},
+          // How the tags suggestions are rendered
+          render: {
+            option: function(item, escape) {
+              return '<div>' +
+                '<span class="title">' +
+                '<span class="name">' + item.text + '</span>' +
+                '</span>' +
+                '</div>';
+            }
+          },
 
-					// How to load new tags
-					load: function(query, callback) {
-						if (!query.length) return callback();
+          // How to load new tags
+          load: function(query, callback) {
+            if (!query.length) return callback();
 
-						if (subscriptions['tags'])
-							subscriptions['tags'].stop();
+            if (subscriptions['tags'])
+              subscriptions['tags'].stop();
 
-						subscriptions['tags'] = Meteor.subscribe('tagsByQuery', { text: { $regex: query, $options: 'i' } });
-						var tags = Tags.find({ text: { $regex: query, $options: 'i' } }).fetch();
+            subscriptions['tags'] = Meteor.subscribe('tagsByQuery', { text: { $regex: query, $options: 'i' } });
+            var tags = Tags.find({ text: { $regex: query, $options: 'i' } }).fetch();
 
-						callback(tags);
-					}
-				}
-			}
-		}
-	},
+            callback(tags);
+          }
+        }
+      }
+    }
+  },
 
-	status: {
-		type: String,
-		optional: true,
-		autoform: {
-			placeholder: "schemaLabel"
-		}
-	},
+  status: {
+    type: String,
+    optional: true,
+    autoform: {
+      placeholder: "schemaLabel"
+    }
+  },
 
-	// The document was created on this date
-	createdOn: {
-		type: Date,
-		optional: false,
+  // The document was created on this date
+  createdOn: {
+    type: Date,
+    optional: false,
     autoValue: function() {
       if (this.isInsert) {
         return new Date;
@@ -109,37 +109,37 @@ NodesScheme = new SimpleSchema({
         this.unset();
       }
     }
-	},
+  },
 
-	// The document was updated on this date
-	lastChanged: {
-		type: Date,
-		optional: false
-	},
+  // The document was updated on this date
+  lastChanged: {
+    type: Date,
+    optional: false
+  },
 
-	// The document was shared by this user
-	userId: {
-		type: String,
-		optional: false
-	},
+  // The document was shared by this user
+  userId: {
+    type: String,
+    optional: false
+  },
 
-	// The parent node id
-	parent: {
-		type: String,
-		optional: false
-	},
+  // The parent node id
+  parent: {
+    type: String,
+    optional: false
+  },
 
-	// Which depth level the node is at
-	level: {
-		type: Number,
-		optional: false
-	},
+  // Which depth level the node is at
+  level: {
+    type: Number,
+    optional: false
+  },
 
-	// Is this node collapsed?
-	collapsed: {
-		type: Boolean,
-		optional: true
-	}
+  // Is this node collapsed?
+  collapsed: {
+    type: Boolean,
+    optional: true
+  }
 
 });
 
