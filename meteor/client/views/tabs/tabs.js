6a58c6c77a69f8d4fa73ce5134164db79e89504f
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-06-02 09:31:30 +0200

    Clean up tabs template.

--- meteor/client/views/tabs/tabs.js
+++ meteor/client/views/tabs/tabs.js
@@ -1,16 +1,99 @@
+////////////////////////////////////////////////////////////////////////////////
+// Tree template logic
+////////////////////////////////////////////////////////////////////////////////
+
+
+var scrollBarWidths = 40;
+
+
+// -- The tabs resize and controller methods -----------------------------------
+
+/**
+ * Return the width of all tabs combined
+ */
+var widthOfList = function() {
+  var itemsWidth = 0;
+
+  $('.list li').each(function() {
+    var it = $(this).outerWidth();
+    itemsWidth = itemsWidth + it;
+  });
+
+  return itemsWidth;
+};
+
+/**
+ * Returns the width of hidden area
+ */
+var widthOfHidden = function() {
+  return (($('.tab-wrapper').outerWidth(true)) - widthOfList() - getLeftPosi()) - scrollBarWidths || 0;
+};
+
+/**
+ * Get the left most position
+ */
+var getLeftPosi = function() {
+  return $('.list') && $('.list').position() && $('.list').position().left;
+};
+
+/**
+ * Readjusts the tab view and renders the scroll indicators
+ */
+reAdjust = function() {
+
+  // if tabs view overflows the container, render a scroll button on the right
+  if (($('.tab-wrapper').outerWidth(true)) <= widthOfList()) {
+    $('.scroller-right').show();
+
+  // else hide it and readjust the view
+  } else {
+    $('.scroller-right').hide();
+
+    if (getLeftPosi() < 0) {
+      $('.list').animate({ left: "-=" + getLeftPosi() + "px" }, 'slow');
+    }
+  }
+
+  // If left position is outside of container, show a scroll button else hide it
+  if (getLeftPosi() < 0) {
+    $('.scroller-left').show();
+  } else {
+    $('.scroller-left').hide();
+  }
+}
+
+
+// -- Template helpers ---------------------------------------------------------
+
+
 Template.Tabs.helpers({
+
     tabs: function() {
-        return Tabs.getTabs();
+        return GV.tabs.getTabs();
     },
+
     isMainDocument: function() {
     	return Session.get('nodeInFocus') == Session.get('mainDocument');
     },
+
     getTitle: function(id) {
-    	var node = GV.collections.Nodes.findOne({_id: id}, { fields: { title:1}});
+    	var node = GV.collections.Nodes.findOne({
+        _id: id
+      },
+      {
+        fields: {
+          title: 1
+        }
+      });
 
     	return node && node.title || null;
     }
+
 });
+
+
+// -- Template events ----------------------------------------------------------
+
 
 Template.Tabs.events({
 
@@ -30,77 +113,38 @@ Template.Tabs.events({
 		event.preventDefault && event.preventDefault();
 		event.stopPropagation && event.stopPropagation();
 
-		// If tab removed, go to the general tab. This should be changed to be the nearest tab.
+		// If tab removed, go to the general tab.
+    // This should be changed to be the nearest tab.
 		Session.set('nodeInFocus', Session.get('mainDocument'));
-		Tabs.removeTab(this);
+		GV.tabs.removeTab(this);
 	},
 
 	'click .scroller-right': function() {
 		$('.scroller-left').fadeIn('slow');
 		$('.scroller-right').fadeOut('slow');
-		$('.list').animate({left:"+="+widthOfHidden()+"px"},'slow',function(){
-
-		});
+		$('.list').animate({left:"+="+widthOfHidden()+"px"},'slow');
 	},
 
 	'click .scroller-left': function() {
 		$('.scroller-right').fadeIn('slow');
 		$('.scroller-left').fadeOut('slow');
-		$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){
-
-		});
+		$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow');
 	},
 
 	'mouseenter .list span.glyphicon': function(event, tmpl) {
 		$(event.target).parent().addClass('hovered');
 	},
+
 	'mouseleave .list span.glyphicon': function(event, tmpl) {
 		$(event.target).parent().removeClass('hovered');
 	}
 
 });
 
-// -- The tabs resize and controller methods ---------------------------------------
 
-var scrollBarWidths = 40;
+//-- Make sure the tab view readjust itself on change --------------------------
 
-var widthOfList = function() {
-  var itemsWidth = 0;
-
-  $('.list li').each(function() {
-    var it = $(this).outerWidth();
-    itemsWidth = itemsWidth + it;
-  });
-
-  return itemsWidth;
-};
-
-var widthOfHidden = function() {
-  return ( ($('.tab-wrapper').outerWidth(true)) - widthOfList() - getLeftPosi() ) - scrollBarWidths || 0;
-};
-
-var getLeftPosi = function() {
-  return $('.list') && $('.list').position() && $('.list').position().left;
-};
-
-reAdjust = function() {
-
-  if (($('.tab-wrapper').outerWidth(true)) <= widthOfList()) {
-    $('.scroller-right').show();
-  } else {
-    $('.scroller-right').hide();
-    if (getLeftPosi() < 0) {
-    	$('.list').animate({left:"-="+getLeftPosi()+"px"},'slow',function(){
-		});
-	}
-  }
-
-  if (getLeftPosi() < 0) {
-    $('.scroller-left').show();
-  } else {
-  	$('.scroller-left').hide();
-  }
-}
+// TODO: maybe a little hacky?
 
 Template.Tabs.rendered = function() {
 	reAdjust();
