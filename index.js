38499fad223c850d1309d35dee6b0534ffdeae40
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-06-30 13:18:16 +0200

    Revert the index.js file

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /*eslint strict:0*/
-/*jshint strict:false */
-
 var childProcess = require('child_process');
 var os = require('os');
 var fs = require('fs');
 var net = require('net');
 var path = require('path');
+var exec = childProcess.exec;
 
 /* App Name */
 /* -------- */
@@ -95,28 +94,9 @@ function start (callback) {
           var mongodPath = path.join(dirname, 'resources', 'mongod');
 
           // Arguments passed to mongod command.
-          var mongodArgs = [
-            '--bind_ip',
-            '127.0.0.1',
-            '--dbpath',
-            dataPath,
-            '--port',
-            mongoPort,
-            '--unixSocketPrefix',
-            dataPath,
-            '--smallfiles'
-          ];
-
+          var mongodArgs = ['--bind_ip', '127.0.0.1', '--dbpath', dataPath, '--port', mongoPort, '--unixSocketPrefix', dataPath, '--smallfiles'];
           if (os.platform() === 'win32') {
-            mongodArgs = [
-              '--bind_ip',
-              '127.0.0.1',
-              '--dbpath',
-              dataPath,
-              '--port',
-              mongoPort,
-              '--smallfiles'
-            ];
+            mongodArgs = ['--bind_ip', '127.0.0.1', '--dbpath', dataPath, '--port', mongoPort, '--smallfiles'];
           }
 
           // Start the Mongo process.
@@ -151,6 +131,7 @@ function start (callback) {
               userEnv.BIND_IP = '127.0.0.1';
               userEnv.DB_PATH = dataPath;
               userEnv.MONGO_URL = 'mongodb://localhost:' + mongoPort + '/meteor';
+              // user_env.METEOR_SETTINGS = fs.readFileSync(path.join(dirname, 'resources', 'settings.json'), 'utf8');
               userEnv.DIR = dirname;
               userEnv.NODE_ENV = 'production';
               userEnv.NODE_PATH = path.join(dirname, 'node_modules');
@@ -173,7 +154,7 @@ function start (callback) {
               nodeChild.stdout.on('data', function (nodeData) {
                 console.log(nodeData);
 
-                if (data.indexOf('Meteor app started.') !== -1) {
+                if (data.indexOf(!'Meteor app started.' === -1)) {
                   if (!opened) {
                     opened = true;
                   } else {
@@ -197,8 +178,7 @@ function start (callback) {
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
 
-// This is to let ESLint know that it's ok to have a global and 'true' allows it to be read/write
-/*global mainWindow:true*/
+/*global mainWindow:true*/ // <-- This is to let ESLint know that it's ok to have a global and 'true' allows it to be read/write
 mainWindow = null;
 
 // Emitted when the application is activated while there is no opened windows.
