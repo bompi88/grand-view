342427814cb877559b7ede517c136b7662379ad7
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-07-02 13:49:37 +0200

    Revert of output path grunt task. The git repo should be loated at Drive:\xxxx"

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     'create-windows-installer': {
       appDirectory: base + '/dist/windows/GrandView',
-      outputDirectory: '/tmp/GrandView/installer',
+      outputDirectory: base + '/tmp/GrandView/installer',
       authors: 'Concept - Bjørn Bråthen, Andreas Drivenes',
       description: 'Et verktøy for å strukturere store informasjonsmengder.',
       exe: 'GrandView_setup.exe'
