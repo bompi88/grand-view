aa373bf1f438ac881a4b409425d14dd5a938d748
Bjørn Bråthen <bjorbrat88@gmail.com>
2017-06-10 11:59:41 +0200

    i386 packages for libgtk2.0-0, libx11-dev and libxkbfile-dev

--- .scripts/travis_before_deploy.sh
+++ .scripts/travis_before_deploy.sh
@@ -8,7 +8,7 @@ if [ `uname` = "Linux" ]; then
   if [ "$ARCH" == "x64" ]; then
     npm run test:build .dist/linux-unpacked/grandview
     mv .dist/${APP}_${VERSION}_amd64.deb .releases/${APP}_${TAG}_amd64.deb;
-    mv .dist/${APP}-${VERSION}-x86_64.AppImage; .releases/${APP}-${TAG}-x86_64.AppImage;
+    mv .dist/${APP}-${VERSION}-x86_64.AppImage .releases/${APP}-${TAG}-x86_64.AppImage;
   elif [ "$ARCH" == "x86" ]; then
     npm run test:build .dist/linux-ia32-unpacked/grandview;
     mv .dist/${APP}_${VERSION}_i386.deb .releases/${APP}_${TAG}_i386.deb;
